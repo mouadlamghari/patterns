2a753b5544817fb5244d6d81974994b2c35f7ebe
mouadLLL <mouaddev0002@gmail.com>
2024-02-19 23:10:00 +0100

    factory

---
+++ Factory.ts
@@ -0,0 +1,58 @@
+interface Product{
+    id : string
+    name:string
+    price : number
+    transaction : string
+}
+
+interface Creator{
+    create(...args:any):Product
+}
+
+class creaTePhone implements Creator{
+    create(id:string,name:string,price:number,transaction:string): Product {
+        return new Phone(id,name,price,transaction)
+    }
+}
+
+class createPc implements Creator {
+    create(id:string,name:string,price:number,transaction:string,ram:number,ssd:number): Product {
+        return new Pc(id,name,price,transaction,ssd,ram)
+    }
+}
+
+class Phone implements Product {
+    public id:string;
+    public name:string;
+    public price:number;
+    public transaction:string;
+
+    constructor(id:string,name:string,price:number,transaction:string){
+        this.id=id
+        this.name=name
+        this.price=price
+        this.transaction=transaction
+    }
+}
+
+class Pc implements Product{
+    public id:string;
+    public name:string;
+    public price:number;
+    public transaction:string;
+    public ram : number;
+    public ssd : number;
+    constructor(id:string,name:string,price:number,transaction:string,ssd:number,ram:number){
+        this.id=id
+        this.name=name
+        this.price=price
+        this.transaction=transaction
+        this.ram=ram
+        this.ssd=ssd
+    }
+}
+
+
+const pc = new createPc()
+let newPc = pc.create("1","mouad",23,"s-qhe34Vhd",20,1)
+
